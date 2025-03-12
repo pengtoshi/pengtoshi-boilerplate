6c364074368146693abbfef5b78142605c211dd4
everGreenGH <skin0011@naver.com>
2025-03-12 14:11:42 +0900

    🔨 Fix: Add on-off options in Interaction (#4)

--- libs/ui/src/lib/components/Display/Interaction/Interaction.tsx
+++ libs/ui/src/lib/components/Display/Interaction/Interaction.tsx
@@ -1,12 +1,20 @@
 import clsx from "clsx";
 import type { UIProps } from "../../../props";
 
-export const Interaction = ({ className, ...props }: UIProps.Div) => {
+export interface InteractionProps extends UIProps.Div {
+  hover?: boolean;
+  focus?: boolean;
+  active?: boolean;
+}
+
+export const Interaction = ({ className, hover = true, focus = true, active = true, ...props }: InteractionProps) => {
   return (
     <div
       className={clsx(
         "absolute inset-0 bg-gray-950 opacity-0 transition-opacity duration-300",
-        "group-hover:opacity-[0.06] group-focus:opacity-[0.06] group-active:opacity-[0.12]",
+        hover && "group-hover:opacity-[0.06]",
+        focus && "group-focus:opacity-[0.06]",
+        active && "group-active:opacity-[0.12]",
         className,
       )}
       {...props}
