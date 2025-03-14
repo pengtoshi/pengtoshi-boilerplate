02d819d141e314021bb073086c8c7569df7c396d
everGreenGH <skin0011@naver.com>
2025-03-14 13:47:39 +0900

    🔨 Fix: Change import error in unit test (#4)

--- libs/ui/src/lib/components/Input/Textarea/Textarea.spec.tsx
+++ libs/ui/src/lib/components/Input/Textarea/Textarea.spec.tsx
@@ -1,7 +1,7 @@
 import { composeStories } from "@storybook/react";
 import "@testing-library/jest-dom";
 import { render } from "@testing-library/react";
-import * as stories from "./TextArea.stories";
+import * as stories from "./Textarea.stories";
 
 describe("TextArea", () => {
   Object.entries(composeStories(stories)).forEach(([, Story]) => {
